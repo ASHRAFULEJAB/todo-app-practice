9d1fd1a04213279a4c28b220a7da7ea0df94be14
Ashraful Islam <19103189@iubat.edu>
2024-02-24 04:22:21 +0600

    inital setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>todo app</h1>
+      <Button>Hello Todo</Button>
+    </>
+  );
+}
+
+export default App;
