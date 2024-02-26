50d597090e675cd6170f719cea3d893d9e69bffe
Ashraful Islam <19103189@iubat.edu>
2024-02-27 04:45:06 +0600

    main layout design

--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -1,11 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    todos:[]
-}
+  todos: [],
+};
+type TTodo = {};
 
-const todoSlice = createSlice ({
-    name:"todo",
-    initialState,
-    reducers:{}
-})
-export default todoSlice.reducer
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<TTodo>) => {
+      state.todos.push({ ...action.payload, isCompleted: false });
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter((item) => item.id !== action.payload);
+    },
+    toggoleComplete: (state, action: PayloadAction<string>) => {
+      const task = state.todos.find((item) => item.id === action.payload);
+      task!.isCompleted = !task?.isCompleted;
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggoleComplete } = todoSlice.actions;
+export default todoSlice.reducer;
